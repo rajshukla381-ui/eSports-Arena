92f9ed9fde3a46384a58f2b0fd468f54841cf87e
Ansh Shukla <raj2404shukla@gmail.com>
2025-09-18 17:38:48 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
+const nextConfig = {};
 
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-            }
-        ],
-    },
-};
-
-const pwaConfig = {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-};
-
-export default withPWA(pwaConfig)(nextConfig);
+export default nextConfig;
