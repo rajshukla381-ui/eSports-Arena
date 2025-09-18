39c3d5af1c3d00b284cff0509ff2e9ea8d46e65e
Ansh Shukla <raj2404shukla@gmail.com>
2025-09-18 12:41:56 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "studio-6736140775-2ec69",
-  appId: "1:941814599781:web:08eb765484fc95490bb68c",
-  storageBucket: "studio-6736140775-2ec69.firebasestorage.app",
-  apiKey: "AIzaSyAgFgZR1Ol1y6yiul-XsgxPx-0o3eGkz_k",
-  authDomain: "studio-6736140775-2ec69.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "941814599781"
+  "projectId": "studio-6736140775-2ec69",
+  "appId": "1:941814599781:web:81f3a24ed534538a0bb68c",
+  "storageBucket": "studio-6736140775-2ec69.firebasestorage.app",
+  "apiKey": "AIzaSyAgFgZR1Ol1y6yiul-XsgxPx-0o3eGkz_k",
+  "authDomain": "studio-6736140775-2ec69.firebaseapp.com",
+  "measurementId": "G-EM53T3C2XD",
+  "messagingSenderId": "941814599781"
 };
 
 // Initialize Firebase
